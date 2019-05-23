d289e81989d3d88931b025d4a6d6c091a208a391
chevo <ktyab05@hotmail.com>
2019-05-23 01:42:41 -0600

    'crudIncompleto'

--- app/src/main/java/com/example/sistemadeencuestas/DataBaseOpenHelper.java
+++ app/src/main/java/com/example/sistemadeencuestas/DataBaseOpenHelper.java
@@ -1,6 +1,7 @@
 package com.example.sistemadeencuestas;
 
 import android.content.Context;
+import android.os.health.PackageHealthStats;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
@@ -8,7 +9,7 @@ public class DataBaseOpenHelper extends SQLiteAssetHelper {
     private static final String DATABASE_NAME="BaseEncuesta.db";
     private static final int DATABASE_VERSION = 1;
 
-    public DataBaseOpenHelper(PregActivity PregActivity, String s, Context context, int i) {
+    public DataBaseOpenHelper( PregActivity pregActivity, String s, Context context, int i) {
         super(context,DATABASE_NAME, null,DATABASE_VERSION);
     }
 
